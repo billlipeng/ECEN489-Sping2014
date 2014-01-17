0491fea89385af3cd067eabcfffb2bcc1cbaab24
toming90 <toming90@gmail.com>
2014-01-17 15:19:16 -0600

    Signed-off-by: toming90 <toming90@gmail.com>

---
+++ yin634/Java1/src/Java1.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Java1 {
+	public static void display(int number1,int number2){
+		System.out.print("what is the sum of ");
+		System.out.println(number1 + "+" + number2 + "?");
+	}
+	public static void main(String[] args) {
+		
+		int CorrectAnswer;
+		int ClientAnswer;
+		Random random1=new Random();
+		int No1=random1.nextInt(500)+1;
+		Random random2=new Random();
+		int No2=random2.nextInt(500)+1;
+		CorrectAnswer = No1+No2;
+		
+		display(No1,No2);
+		boolean status = true;
+		while(status){
+		System.out.print("Please Enter Your Answer:");
+		Scanner input = new Scanner(System.in);
+		ClientAnswer=input.nextInt();
+		if(ClientAnswer == CorrectAnswer)
+			{System.out.println("^_^ Your Answer is CORRECT!");
+				status = false;
+			}
+		else
+			System.out.println("T_T Your Answer is WRONG!");
+		}
+	}
+
+}
