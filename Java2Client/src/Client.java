189b17d5b02505121ec797486efba545ca0d6580
samcarey <samcarey@tamu.edu>
2014-01-20 14:34:53 -0600

    Client. Connects to localhost.

---
+++ Java2Client/src/Client.java
@@ -0,0 +1,58 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Client {
+	public static void main(String []args){
+		Random rand = new Random();
+		int num1 = rand.nextInt(10);
+		int num2 = rand.nextInt(10);
+		int sum = 0;
+		
+		try{
+			System.out.println("Client connecting...");
+			Socket connection = new Socket(InetAddress.getByName("127.0.0.1"), 2015);
+			System.out.println("Client connected!");
+			
+			ObjectOutputStream output = new ObjectOutputStream(connection.getOutputStream());
+			output.writeInt(num1);
+			output.writeInt(num2);
+			output.flush();
+			
+			ObjectInputStream input = new ObjectInputStream(connection.getInputStream());
+			sum = input.readInt();
+			
+			input.close();
+			output.close();
+			connection.close();
+		}catch(IOException e){
+			System.out.println("IOException");
+		}
+
+		Scanner sc = new Scanner(System.in);
+		
+		while (true){
+			try{
+				System.out.println("What is " + num1 + "+" + num2 + "?");
+				System.out.print("> ");
+				
+				if (sc.nextInt() == sum){
+					System.out.println("Correct!");
+					break;
+				}else{
+					System.out.println("Incorrect!");
+				}
+			}
+			catch(InputMismatchException e){
+				System.out.println("Your answer is garbage!");
+				sc.next();
+			}
+		}
+		sc.close();
+	}
+}
