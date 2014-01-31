a74728d57bb51fb124541afdcb31fe132e66f536
Zachary Partal <zpartal@gmail.com>
2014-01-31 08:34:18 +0000

    Completed Opt-Out for HW4

--- zpartal/HW4/AndroidClient/src/com/zpartal/androidclient/MainActivity.java
+++ zpartal/HW4/AndroidClient/src/com/zpartal/androidclient/MainActivity.java
@@ -1,17 +1,78 @@
 package com.zpartal.androidclient;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.UUID;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.bluetooth.*;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
+	private static final String TAG = "AndroidClient";
+	private BluetoothAdapter mBtAdapter;
+	public BluetoothDevice mDevice;
+	private BluetoothSocket mSock;
+	private DataInputStream in;
+	private DataOutputStream out;
+	private Button button;
+	private TextView echoText;
+	private EditText textSend;	
+	private String message = "Hello";
+	private String reply = "";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		button = (Button) findViewById(R.id.button_send);
+		echoText = (TextView) findViewById(R.id.text_echo);
+		textSend = (EditText) findViewById(R.id.text_send);
+		connectBluetooth();
 	}
 
+	private void connectBluetooth() {
+		mBtAdapter = BluetoothAdapter.getDefaultAdapter();
+		mBtAdapter.startDiscovery();
+		while(true){
+			if (mBtAdapter.isDiscovering() == true) {
+                mBtAdapter.cancelDiscovery();
+                break;
+            }
+		}
+		mDevice = mBtAdapter.getRemoteDevice("00:19:15:66:9E:A2");
+		UUID uuid = UUID.fromString("b0d042fe-8a3c-11e3-b91b-d231feb1dc81");
+		try {
+			mSock = mDevice.createRfcommSocketToServiceRecord(uuid);
+			mSock.connect();
+			in = new DataInputStream(mSock.getInputStream());
+			out = new DataOutputStream(mSock.getOutputStream());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}		
+		
+		button.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				message = textSend.getText().toString();
+				try {
+					out.writeUTF(message);
+					reply = in.readUTF();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				echoText.setText(reply);				
+			}
+		});
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
--- zpartal/HW4/JavaServer/src/JavaServer.java
+++ zpartal/HW4/JavaServer/src/JavaServer.java
@@ -1,9 +1,33 @@
+import java.io.*;
+import javax.bluetooth.*;
+import javax.microedition.io.*;
 
 public class JavaServer {
-
+	public final UUID uuid = new UUID ("b0d042fe8a3c11e3b91bd231feb1dc81", false);
+	public String name = "Java Server";
+	public final String url = "btspp://localhost:" + uuid + ";name=" + name + ";authenticate=false;encrypt=false;" ;
+	
+	StreamConnectionNotifier service = null;
+	StreamConnection conn = null;
+	
+	public JavaServer() {
+		try {
+			System.out.println("Initializing Server...");
+			service = (StreamConnectionNotifier) Connector.open(url);
+			conn = (StreamConnection) service.acceptAndOpen();
+			System.out.println("Client connected...");
+			DataInputStream in = new DataInputStream(conn.openInputStream());
+			DataOutputStream out = new DataOutputStream(conn.openOutputStream());
+			while (true) {
+				String received = in.readUTF();
+				System.out.println("Received: " + received);
+				out.writeUTF("Echo: " + received);
+			}			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}		
+	}	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		JavaServer javaserver = new JavaServer();
 	}
-
 }
