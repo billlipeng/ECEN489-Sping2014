5156200ca756037b1aac7a630461ad2142b0d360
Zachary Partal <zpartal@gmail.com>
2014-02-16 20:00:38 -0600

    server stuff

---
+++ Project1/Team3/Team3Server/src/Worker.java
@@ -0,0 +1,48 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import com.zpartal.project1.datapackets.*;
+
+
+public class Worker implements Runnable {
+	
+	private Socket client;	
+	static private ObjectInputStream ois;
+//	static private ObjectOutputStream oos;
+		
+	public Worker(Socket client) {
+		super();
+		this.client = client;
+	}
+	
+	@Override
+	public void run() {
+		try {
+//			oos = new ObjectOutputStream(client.getOutputStream());
+			ois = new ObjectInputStream(client.getInputStream());
+			
+			while(true) {
+//				ClientPacket cp = (ClientPacket) ois.readObject();
+//				System.out.print("Request from: " + cp.getClientID());
+//				System.out.println("; Data: " + cp.getNum1() + ", " + cp.getNum2());
+//				ServerPacket response = new ServerPacket("zpartal", cp.getNum1() + cp.getNum2());
+//				oos.flush();
+//				oos.writeObject(response);
+
+
+
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+//			oos.close();
+			ois.close();
+			client.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
