9e16715f8cf0aff1d278ffbed53dcb848b3bface
Cameron Shaw <cameroncowden@gmail.com>
2014-02-16 21:31:51 -0600

    Project 1 Java Server and SQL Connection

---
+++ Project1/Team1/JavaServer/src/project1JavaServer/DataThreader.java
@@ -0,0 +1,109 @@
+package project1JavaServer;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import project1JavaServer.dataRow;
+
+public class DataThreader extends Thread{
+
+	
+	
+		protected Socket socket;
+		dataRow row;
+		//declare secondary obj here for send if desired. 
+		public DataThreader(Socket client){
+			this.socket = client;
+		}
+		public void run(){
+			
+			//ObjectOutputStream sender = null; //Unneeded right now
+			ObjectInputStream receiver = null;
+			
+			//Declaring data
+			
+			try{
+				//sender = new ObjectOutputStream(socket.getOutputStream()); //Unneeded right now
+				receiver = new ObjectInputStream(socket.getInputStream());					
+			}
+			catch(IOException e){
+				System.err.println("Could not create I/O Streams..!");
+			}
+			Connection connection = null;
+			
+				try{
+					while(true){
+				//listening for packets
+					connection = DriverManager.getConnection("jdbc:sqlite:DataReadings.db");
+					dataRow row = (dataRow) receiver.readObject();					
+				
+					System.out.println("Attempting connection to SQL server.");
+					
+					try {
+						connection.setAutoCommit(false);
+						String insert = "INSERT INTO Data(sensorID, teamID, runID, latitude, longitude, date, time, sensorValue, sensorType, attribute) VALUES(?,?,?,?,?,?,?,?,?,?)";
+						PreparedStatement insertData = connection.prepareStatement(insert);
+						insertData.setString(1, row.sensorID);
+						insertData.setString(2, row.teamID);
+						insertData.setInt(3, row.runID);
+						insertData.setDouble(4, row.latitude);
+						insertData.setDouble(5, row.longitude);
+						insertData.setString(6, row.date);
+						insertData.setString(7, row.time);
+						insertData.setFloat(8, row.sensorValue);
+						insertData.setString(9, row.sensorType);
+						insertData.setString(10, row.attribute);
+						insertData.executeUpdate();
+						connection.commit();
+						connection.setAutoCommit(true);
+					}
+					catch (SQLException e) {
+						System.err.println(e.getMessage());
+					}
+					
+					//System.exit(0);
+				}
+					
+					//Now has data read into row[].
+				}
+				catch(ClassNotFoundException e){
+					System.err.println("Class not found...");
+				}
+				catch(IOException e){
+					
+					System.out.println("A client disconnected!");
+				}
+				catch(SQLException e){
+					System.out.println("SQL Exception!");
+					System.err.println(e.getMessage());
+				}
+				try{
+					socket.close();
+					System.out.println("Socket terminated.");
+					connection.close();
+					
+				}
+				catch(IOException e){
+					System.err.println("Couldn't close the socket!");
+				}
+				catch(SQLException e){
+					System.out.println("SQL Exception failed trying to close!");
+					System.err.println(e.getMessage());
+				}
+				
+			}
+			
+
+	}
+
+
---
+++ Project1/Team1/JavaServer/src/project1JavaServer/dataRow.java
@@ -0,0 +1,14 @@
+package com.mhardiman.gps_retrieval;
+public class dataRow
+{
+	public String sensorID;
+	public static String teamID = "Team1";
+	public int runID;
+	public double latitude;
+	public double longitude;
+	public String date;
+	public String time;
+	public float sensorValue;
+	public String sensorType;
+	public String attribute; //start, stop, sensor reading or not
+}
