43604179e4724914b4a7e6b9f61b28b92a47ce7a
Jeff Terrell <jdterrell@neo.tamu.edu>
2014-02-19 15:03:54 -0600

    I have no commit message.

---
+++ Project1/Team5/Sensor5/Sensor5.ino
@@ -0,0 +1,51 @@
+
+#include <MeetAndroid.h>
+#include <aJSON.h>
+
+#define SENSOR_PIN A0
+
+MeetAndroid meetAndroid;
+int onboardLed = 13;
+double sourceVoltage = 3.8;
+
+void setup()  
+{
+
+  Serial1.begin(57600); 
+  Serial.begin(57600);
+  
+  meetAndroid.registerFunction(sensorEvent, 's');
+  meetAndroid.registerFunction(sensorEvent, 'S');
+  
+  pinMode(onboardLed, OUTPUT);
+  digitalWrite(onboardLed, HIGH);
+
+}
+
+void loop()
+{
+  meetAndroid.receive(); // you need to keep this in your loop() to receive events
+}
+
+void sensorEvent(byte flag, byte numOfValues)
+{
+  Serial.println("test");
+  float rawVal = analogRead(SENSOR_PIN);
+  float sensorVoltage = rawVal*(sourceVoltage/1023);
+  
+  
+  aJsonObject *sensorData;
+  sensorData=aJson.createObject();  
+  aJson.addItemToObject(sensorData, "sensor-id", aJson.createItem("Sensor 5"));
+  aJson.addItemToObject(sensorData, "sensor-type", aJson.createItem("Photo"));
+  aJson.addItemToObject(sensorData, "sensor-value", aJson.createItem(sensorVoltage));
+  
+  char* jsonString = aJson.print(sensorData);
+  if (jsonString != NULL)  {
+    Serial.println(jsonString);
+  }
+  
+  meetAndroid.send(jsonString);
+  
+}
+
