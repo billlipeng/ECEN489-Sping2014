2e82e1364ed7a6d72ba739791eb25fa1734aa5d0
toming90 <toming90@gmail.com>
2014-02-21 12:45:26 -0600

    Project1_Team2_V1_2

--- Project1/Team2/Project1_Team2_V1_2/src/com/example/project1_team2_v1_2/Team2Proj.java
+++ Project1/Team2/Project1_Team2_V1_2/src/com/example/project1_team2_v1_2/Team2Proj.java
@@ -9,9 +9,15 @@ import java.net.UnknownHostException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import com.google.gson.Gson;
 import com.mfa157.project1packet.Project1Packet;
+
+
+
+
+
 
 
 
@@ -26,13 +32,18 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.res.Configuration;
 import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import at.abraxas.amarino.Amarino;
 import at.abraxas.amarino.AmarinoIntent;
 
@@ -40,8 +51,14 @@ public class Team2Proj extends Activity {
 	ObjectOutputStream os;
 	ObjectInputStream is;
 	private Button btnRec, btnEnd, btnGPS, btnSend,btnReset;
-	private String DEVICE_ADDRESS = "00:15:FF:F2:10:0F";
+	private String DEVICE_ADDRESS ;
 
+	
+	private List<String> DeviceAddr_List = new ArrayList<String>();
+	private TextView show,text, tvData;
+	
+	private Spinner team_spinner;
+	private ArrayAdapter<CharSequence> adapter;
 	private LocationManager locationManager;
 	private EditText etIP;
 	private EditText etPort;
@@ -51,12 +68,16 @@ public class Team2Proj extends Activity {
 	boolean sensor_flag = false;
 	double lat, lng;
 	String v1,v2;
+	String timestamp, date,runtime;
 	double v3;
 	int i = 0;
+	int startcheck = 0;
+	int check = 0;
+	boolean End;
 	LocationListener locationListener;
-	uploadfusion uf = new uploadfusion();
 	
-	int port = 5555;
+	
+	int port ;
 	String hostip;
 	Socket clientSocket;
 	
@@ -70,9 +91,9 @@ public class Team2Proj extends Activity {
 	ArrayList<String> latitude = new ArrayList<String>();
 	ArrayList<String> longitude = new ArrayList<String>();
 
-	// ArrayList <String> sensor_value = new ArrayList<String>();
-	// ArrayList <String> sensor_value = new ArrayList<String>();
-	// ArrayList <String> sensor_value = new ArrayList<String>();
+	ArrayList <String> ClientID = new ArrayList<String>();
+	ArrayList <String> RunID = new ArrayList<String>();
+	ArrayList <String> Attribute = new ArrayList<String>();
 	
 	private static final String TAG = "Team2P1";
 
@@ -82,8 +103,75 @@ public class Team2Proj extends Activity {
 		setContentView(R.layout.activity_team2_proj);
 		etIP = (EditText) findViewById(R.id.etIP);
 		etPort = (EditText) findViewById(R.id.etPort);
+		tvData = (TextView) findViewById(R.id.tvData);
+		DeviceAddr_List.add("00:12:09:13:99:42");
+		DeviceAddr_List.add("00:15:FF:F2:10:0F");
+		DeviceAddr_List.add("00:12:09:25:92:95");
+		DeviceAddr_List.add("00:12:09:25:92:92");
+		DeviceAddr_List.add("00:12:09:25:96:92");
+		text = (TextView) findViewById(R.id.text);
+		team_spinner = (Spinner) findViewById(R.id.bluetooth_address);
+		adapter = ArrayAdapter.createFromResource(
+				this, R.array.BlueToothAddress, android.R.layout.simple_spinner_item);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		team_spinner.setAdapter(adapter);
+		
+		team_spinner.setOnItemSelectedListener(new OnItemSelectedListenerImp());
 	}
 
+	
+	
+	private class OnItemSelectedListenerImp implements OnItemSelectedListener {
+		public void onItemSelected(AdapterView<?> parent, View view,int position,long id){
+			show = (TextView) findViewById(R.id.show);
+			check++;
+			String str = "The Blue Tooth Address you will connect is: ";
+			if(check>1){
+			String value = parent.getItemAtPosition(position).toString();
+			show.setText(value);
+			if (value.equals("Team1_Address"))
+			{
+				DEVICE_ADDRESS = DeviceAddr_List.get(0);
+				show.setText(str+DEVICE_ADDRESS);
+			}
+			
+			else if (value.equals("Team2_Address"))
+			{
+				DEVICE_ADDRESS = DeviceAddr_List.get(1);
+				show.setText(str+DEVICE_ADDRESS);
+			}
+			
+			else if (value.equals("Team3_Address"))
+			{
+				DEVICE_ADDRESS = DeviceAddr_List.get(2);
+				show.setText(str+DEVICE_ADDRESS);
+			}
+			else if (value.equals("Team4_Address"))
+			{
+				DEVICE_ADDRESS = DeviceAddr_List.get(3);
+				show.setText(str+DEVICE_ADDRESS);
+			}
+			else
+			{
+				DEVICE_ADDRESS = DeviceAddr_List.get(4);
+				show.setText(str+DEVICE_ADDRESS);
+			}
+			}
+			
+		}
+
+		@Override
+		public void onNothingSelected(AdapterView<?> arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+	}
+		
+	
+	public void onConfigurationChanged(Configuration newConfig){
+		super.onConfigurationChanged(newConfig);
+	}
+		
 	protected void onStart() {
 		super.onStart();
 		registerReceiver(arduinoReceiver, new IntentFilter(AmarinoIntent.ACTION_RECEIVED));
@@ -105,11 +193,17 @@ public class Team2Proj extends Activity {
 		sensorType.clear();
 		sensorValue.clear();
 		sensorID.clear();
+		ClientID.clear();
+		RunID.clear();
+		Attribute.clear();
+		show.setText("Reset the data!");
+		tvData.setText("Points:"+latitude.size()+" Time: "+timestamp);
 		
 	}
 
 	public void gps(View v) {
-		sensor_flag = false;
+		startcheck = 0;
+		
 		btnGPS = (Button) findViewById(R.id.start);
 		locationListener = new LocationListener(){
 				@Override
@@ -121,35 +215,25 @@ public class Team2Proj extends Activity {
 
 				@Override
 				public void onLocationChanged(Location location) {
-					if (sensor_flag){
-						if (location != null) {
-							lat = location.getLatitude();
-							lng = location.getLongitude();
-							latitude.add(String.valueOf(lat));
-							longitude.add(String.valueOf(lng));
-							SimpleDateFormat ts = new SimpleDateFormat("HH:mm:ss");
-							String timestamp = ts.format(new Date());
-							SimpleDateFormat dt = new SimpleDateFormat("yyyy-MM-dd");
-							String date = dt.format(new Date());
-
-							time_stamp.add(timestamp);
-							time_date.add(date);
-							Log.i(TAG, String.valueOf(lat) + " " + String.valueOf(lng)+" "+v1+" "+v2+" "+v3);
-							sensor_flag = false;
-
-						}
-
-						else {
-							//double lat = 2;
-							//double lng = 2;
+					
+						try {
+							if (location != null) {
 							
+							if (startcheck<1)
+							{
+								Attribute.add("Start Point");
+								SimpleDateFormat ts = new SimpleDateFormat("HH:mm:ss");
+								runtime = ts.format(new Date());
+								startcheck++;
+								Log.i("TAG","Start Point");
+							}
 							
-							Log.i(TAG, String.valueOf(lat) + " " + String.valueOf(lng)+" "+v1+" "+v2+" "+v3);
-							sensor_flag = false;
-						}
-				}
-					else{
-						if (location != null) {
+							else
+							{
+								Attribute.add("Point");
+								Log.i("TAG","Data Point");
+							}
+							ClientID.add("Team2");
 							lat = location.getLatitude();
 							lng = location.getLongitude();
 							sensorType.add(null);
@@ -158,13 +242,17 @@ public class Team2Proj extends Activity {
 							latitude.add(String.valueOf(lat));
 							longitude.add(String.valueOf(lng));
 							SimpleDateFormat ts = new SimpleDateFormat("HH:mm:ss");
-							String timestamp = ts.format(new Date());
+							timestamp = ts.format(new Date());
 							SimpleDateFormat dt = new SimpleDateFormat("yyyy-MM-dd");
-							String date = dt.format(new Date());
-
+							date = dt.format(new Date());
+							String temp = "Team2_" + date +" _ "+ runtime;  
+							
 							time_stamp.add(timestamp);
 							time_date.add(date);
+							RunID.add(temp);
 							Log.i(TAG, String.valueOf(lat) + " " + String.valueOf(lng)+" "+v1+" "+v2+" "+v3);
+							show.setText("Collecting GPS Data");
+							tvData.setText("Points:"+latitude.size()+" Time: "+timestamp);
 						}
 
 						else {
@@ -172,7 +260,9 @@ public class Team2Proj extends Activity {
 							double lng = 1;
 							Log.i(TAG, String.valueOf(lat) + " " + String.valueOf(lng)+" "+v1+" "+v2+" "+v3);
 						}
-					}
+						}catch ( Exception e ){
+							e.printStackTrace();
+						}
 				}
 
 				@Override
@@ -190,8 +280,9 @@ public class Team2Proj extends Activity {
 
 				locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 				locationManager.requestLocationUpdates(
-						LocationManager.GPS_PROVIDER, 1000, 0,
+						LocationManager.GPS_PROVIDER, 3000, 0,
 						locationListener);
+				show.setText("Collecting GPS Data");
 	}
 
 	
@@ -201,7 +292,7 @@ public class Team2Proj extends Activity {
 	 public void receive(View v){
 	 btnRec = (Button) findViewById(R.id.btn1);
 	 
-	 GPS_status = false;
+	 
 	 receive_status = true;
 	 
 	 new AsyncTask<Void, Void, Void> (){
@@ -226,7 +317,27 @@ public class Team2Proj extends Activity {
 
 	public void end(View v) {
 		btnEnd = (Button) findViewById(R.id.end);
+		
 		locationManager.removeUpdates(locationListener);
+			ClientID.add("Team2");
+			Attribute.add("End Point");
+			sensorType.add(null);
+			sensorID.add(null);
+			sensorValue.add(null);
+			latitude.add(String.valueOf(lat));
+			longitude.add(String.valueOf(lng));
+			SimpleDateFormat ts = new SimpleDateFormat("HH:mm:ss");
+			timestamp = ts.format(new Date());
+			SimpleDateFormat dt = new SimpleDateFormat("yyyy-MM-dd");
+			date = dt.format(new Date());
+			String temp = "Team2_" + date +" _ "+ runtime; 
+			RunID.add(temp);
+			time_stamp.add(timestamp);
+			time_date.add(date);
+			
+			Log.i("TAG","End Point");
+			tvData.setText("Points:"+latitude.size()+" Time: "+timestamp);
+		show.setText("Stop Collecting GPS Information");
 		
 		
 	}
@@ -265,17 +376,25 @@ public class Team2Proj extends Activity {
 						sensorID.add(v1);
 						sensorType.add(v2);
 						sensorValue.add(v3);
+						
+						ClientID.add("Team2");
 						latitude.add(String.valueOf(lat));
 						longitude.add(String.valueOf(lng));
+						Attribute.add("Sensor");
 						SimpleDateFormat ts = new SimpleDateFormat("HH:mm:ss");
-						String timestamp = ts.format(new Date());
+						timestamp = ts.format(new Date());
 						SimpleDateFormat dt = new SimpleDateFormat("yyyy-MM-dd");
-						String date = dt.format(new Date());
+						date = dt.format(new Date());
+						String temp = "Team2_" + date +" _ "+ runtime; 
+						RunID.add(temp);
 						time_stamp.add(timestamp);
 						time_date.add(date);
-						sensor_flag = true;
-						GPS_status = true;
+						Log.i("TAG","Sensor");
+						Log.i(TAG, String.valueOf(lat) + " " + String.valueOf(lng)+" "+v1+" "+v2+" "+v3);
+						
+						tvData.setText("Points:"+latitude.size()+" Time: "+timestamp);
 						receive_status = false;
+						show.setText("Sensor Data Received!");
 						
 					}
 
@@ -298,6 +417,7 @@ public class Team2Proj extends Activity {
 	public void send(View v){
 		btnSend = (Button) findViewById(R.id.send);
 		hostip = etIP.getText().toString();
+		port = Integer.parseInt(etPort.getText().toString());
 		//String[] lat_array = (String[])latitude.toArray(new String[size_lat]);
 		//String[] lng_array = (String[])longitude.toArray(new String[size_lng]);
 		//String[] sensorType_array = (String[])sensorType.toArray(new String[size_sensorType]);
@@ -306,7 +426,7 @@ public class Team2Proj extends Activity {
 		//String[] ts_array = (String[])time_stamp.toArray(new String[size_ts]);
 		//String[] dt_array = (String[])time_date.toArray(new String[size_dt]);
 		
-		/*
+		
 		 new AsyncTask<Void, Void, Void> (){
 			 protected Void doInBackground(Void... params) {
 			    try {
@@ -318,6 +438,9 @@ public class Team2Proj extends Activity {
 					int size_sensorValue = sensorValue.size();
 					int size_ts = time_stamp.size();
 					int size_dt = time_date.size();
+					int size_ClientID = ClientID.size();
+					int size_RunID = RunID.size();
+					int size_Attribute = Attribute.size();
 					
 			    	String[] lat_array = (String[])latitude.toArray(new String[size_lat]);
 					String[] lng_array = (String[])longitude.toArray(new String[size_lng]);
@@ -326,11 +449,14 @@ public class Team2Proj extends Activity {
 					Double[] sensorValue_array = (Double[])sensorValue.toArray(new Double[size_sensorValue]);
 					String[] ts_array = (String[])time_stamp.toArray(new String[size_ts]);
 					String[] dt_array = (String[])time_date.toArray(new String[size_dt]);
+					String[] ClientID_array = (String[])ClientID.toArray(new String[size_ClientID]);
+					String[] RunID_array = (String[])RunID.toArray(new String[size_RunID]);
+					String[] Attribute_array = (String[])Attribute.toArray(new String[size_Attribute]);
 					
 					Log.i(TAG,"Connecting");
 					clientSocket = new Socket(hostip,port);
 					os = new ObjectOutputStream(clientSocket.getOutputStream());
-					Project1Packet Send = new Project1Packet(sensorValue_array, sensorID_array, sensorType_array, lat_array, lng_array, ts_array, dt_array);
+					Project1Packet Send = new Project1Packet(sensorValue_array, sensorID_array, sensorType_array, lat_array, lng_array, ts_array, dt_array, ClientID_array, RunID_array,Attribute_array);
 					os.writeObject(Send);
 					Log.i(TAG, "Data Sent!");
 					os.flush();
@@ -348,30 +474,15 @@ public class Team2Proj extends Activity {
 			    
 			    
 			    return null; }
+			 
+			 protected void onPostExecute(Void result) {
+				 show.setText("DATA SENT!");
+			 };
+			 
 		 }.execute();
-			*/   
-		
-		//for fusion table
-		    
-		    int size_lat = latitude.size();
-			int size_lng = longitude.size();
-			int size_sensorType = sensorType.size();
-			int size_sensorID = sensorID.size();
-			int size_sensorValue = sensorValue.size();
-			int size_ts = time_stamp.size();
-			int size_dt = time_date.size();
 			
-	    	String[] lat_array = (String[])latitude.toArray(new String[size_lat]);
-			String[] lng_array = (String[])longitude.toArray(new String[size_lng]);
-			String[] sensorType_array = (String[])sensorType.toArray(new String[size_sensorType]);
-			String[] sensorID_array = (String[])sensorID.toArray(new String[size_sensorID]);
-			Double[] sensorValue_array = (Double[])sensorValue.toArray(new Double[size_sensorValue]);
-			String[] ts_array = (String[])time_stamp.toArray(new String[size_ts]);
-			String[] dt_array = (String[])time_date.toArray(new String[size_dt]);
-		    
-		    
-		    uf.update(ts_array,dt_array,lat_array,lng_array,sensorID_array,sensorType_array,sensorValue_array);
-		
+		 tvData.setText("Points:"+latitude.size()+" Time: "+timestamp);
+
 		
 	}
 
@@ -425,6 +536,10 @@ public class Team2Proj extends Activity {
 		getMenuInflater().inflate(R.menu.team2_proj, menu);
 		return true;
 	}
+
+
+
+	
 
 	
 
