e24d8fb1747209f3f4eed257c5bbf6b34919f183
IntrepidNomad <jason.tyler.moore@gmail.com>
2014-03-01 22:48:47 -0600

    Linear Interpolation Class- Should work but not tested.

--- Project2/TeamMAP/TeamMAPAnalysis/linearinterpolation.java
+++ Project2/TeamMAP/TeamMAPAnalysis/linearinterpolation.java
@@ -15,14 +15,33 @@ public class linearinterpolation {
 		this.latitude2 = latitude2;
 	}
 	
-	long[] analyze(){
+	double[] analyze(){
+		
+		double longitude1R = toRadians(longitude1);
+		double latitude1R= toRadians(latitude1);
+		double longitude2R= toRadians(longitude2);
+		double latitude2R= toRadians(latitude2);
+		double[] latlongArray= new double[20];
 		
 		//implement haversine function
-		double h= pow(((latitude2-latitude1)/2),2)+cos(latitude1)*cos(latitude2)*pow(sin((longitude2-longitude1)/2),2);
+		double h= pow(((latitude2R-latitude1R)/2),2)+cos(latitude1R)*cos(latitude2R)*pow(sin((longitude2R-longitude1R)/2),2);
 		distance = (2*radiusearth*asin(sqrt(h)));
 		
+		//calculate distance interval between points
+		double interval= distance/10;
+		double angulardistanceR= interval/radiusearth;		
+		//calculate the forward azimuth(bearing)		
+		double forwardazimuthR = acos((sin(latitude2R)-sin(latitude1R)*cos(distance))/(sin(distance)*cos(latitude1R)));
 		
-		return null;
+		//populate double array that contains [longitude,latitude....] values for each of the 10 points
+		for (int i =0; i<10;i++) {
+			latlongArray[2*(i+1)-2]= longitude1R+ atan2(sin(forwardazimuthR)*sin(angulardistanceR)*cos(latitude1R),cos(angulardistanceR)-sin(latitude1R)*sin(latitude2R));
+			latlongArray[2*(i+1)-1]= asin(sin(latitude1R)*cos(angulardistanceR)+ cos(latitude1R)*sin(angulardistanceR)*cos(forwardazimuthR));
+			longitude1R= latlongArray[2*(i+1)-2];
+			latitude1R= latlongArray[2*(i+1)-1];
+		}
+		
+		return latlongArray;
 		
 	}
 	
