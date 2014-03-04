9ea5ebd164a03de8228d8381503d3809a54062f1
IntrepidNomad <jason.tyler.moore@gmail.com>
2014-03-03 19:34:40 -0600

    Updated output to degrees instead of radians- Tested it, it works

--- Project2/TeamMAP/TeamMAPAnalysis/linearinterpolation.java
+++ Project2/TeamMAP/TeamMAPAnalysis/linearinterpolation.java
@@ -2,17 +2,17 @@ import static java.lang.Math.*;
 
 public class linearinterpolation {
 	public double distance;
-	public long longitude1;
-	public long latitude1;
-	public long longitude2;
-	public long latitude2;
+	public double longitude1;
+	public double latitude1;
+	public double longitude2;
+	public double latitude2;
 	public long radiusearth = 6378;
-	linearinterpolation(long longitude1, long latitude1, long longitude2, long latitude2){
+	linearinterpolation(double longitude12, double latitude12, double longitude22, double latitude22){
 		
-		this.longitude1= longitude1;
-		this.latitude1= latitude1;
-		this.longitude2= longitude2;
-		this.latitude2 = latitude2;
+		this.longitude1= longitude12;
+		this.latitude1= latitude12;
+		this.longitude2= longitude22;
+		this.latitude2 = latitude22;
 	}
 	
 	double[] analyze(){
@@ -35,11 +35,12 @@ public class linearinterpolation {
 		
 		//populate double array that contains [longitude,latitude....] values for each of the 10 points
 		for (int i =0; i<10;i++) {
-			latlongArray[2*(i+1)-2]= longitude1R+ atan2(sin(forwardazimuthR)*sin(angulardistanceR)*cos(latitude1R),cos(angulardistanceR)-sin(latitude1R)*sin(latitude2R));
-			latlongArray[2*(i+1)-1]= asin(sin(latitude1R)*cos(angulardistanceR)+ cos(latitude1R)*sin(angulardistanceR)*cos(forwardazimuthR));
-			longitude1R= latlongArray[2*(i+1)-2];
-			latitude1R= latlongArray[2*(i+1)-1];
+			longitude1R= longitude1R+ atan2(sin(forwardazimuthR)*sin(angulardistanceR)*cos(latitude1R),cos(angulardistanceR)-sin(latitude1R)*sin(latitude2R));
+			latitude1R= asin(sin(latitude1R)*cos(angulardistanceR)+ cos(latitude1R)*sin(angulardistanceR)*cos(forwardazimuthR));
+			latlongArray[2*(i+1)-2]= toDegrees(longitude1R);
+			latlongArray[2*(i+1)-1]= toDegrees(latitude1R);
 			forwardazimuthR = acos((sin(latitude2R)-sin(latitude1R)*cos(distance))/(sin(distance)*cos(latitude1R)));
+			
 		}
 		
 		return latlongArray;
