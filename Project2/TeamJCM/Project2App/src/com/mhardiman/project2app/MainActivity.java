2d4a3061acdaf0d87166920419f721a7f4d26c72
mtn7450 <mtn7450@tamu.edu>
2014-03-04 21:21:31 -0600

    App update.

--- Project2/TeamJCM/Project2App/src/com/mhardiman/project2app/MainActivity.java
+++ Project2/TeamJCM/Project2App/src/com/mhardiman/project2app/MainActivity.java
@@ -24,6 +24,10 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.location.*;
 import android.content.BroadcastReceiver;
 import android.content.Intent;
@@ -38,16 +42,19 @@ public class MainActivity extends Activity{
 	Button speechButton;
 	TextView runningText;
 	boolean running = false;
-	myLocationListener myLL;
+	myListener myListen;
 	TextView ipText;
 	TextView portText;
 	Handler timerHandler = new Handler();
+	int index = 0;
     Runnable timerRunnable = new Runnable() 
     {
     	 @Override
          public void run() {
-             myLL.getCoordinates();
              timerHandler.postDelayed(this, 10000);
+             System.out.println(index++);
+            	 myListen.getCoordinates((index-1)%1==0);
+
     	 }
     };
     	 
@@ -58,7 +65,10 @@ public class MainActivity extends Activity{
 		runningText = (TextView)findViewById(R.id.runningText);
 		ipText = (TextView)findViewById(R.id.ipText);
 		portText = (TextView)findViewById(R.id.portText);
-		myLL = new myLocationListener(this, (ListView)findViewById(R.id.dataText));
+		
+		SensorManager mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+		
+		myListen = new myListener(this, (ListView)findViewById(R.id.dataText), mSensorManager);
 		PackageManager pm = getPackageManager();
 		List<ResolveInfo> activities = pm.queryIntentActivities(new Intent(
 	    RecognizerIntent.ACTION_RECOGNIZE_SPEECH), 0);
@@ -75,7 +85,7 @@ public class MainActivity extends Activity{
 	{
 		  if (requestCode == REQUEST_CODE & resultCode == RESULT_OK) {
 			  ArrayList<String> matches = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-			  for (int i=0; i<3; i++)
+			  for (int i=0; i<matches.size(); i++)
 			  {
 				  String currentPhrase = matches.get(i);
 			  	if ((!running && currentPhrase.toLowerCase().contains("start")) | (running && currentPhrase.toLowerCase().contains("stop")))
@@ -90,7 +100,7 @@ public class MainActivity extends Activity{
 					if (ip.length()<=7 | port.length() < 4)
 						Toast.makeText(this, "Please enter IP and port information.", Toast.LENGTH_SHORT).show();
 					else
-						myLL.sendData(ipText.getText().toString(), portText.getText().toString());
+						myListen.sendData(ipText.getText().toString(), portText.getText().toString());
 					break;
 			  	}
 			  	
@@ -118,6 +128,7 @@ public class MainActivity extends Activity{
 	          timerHandler.postDelayed(timerRunnable, 0);
 	          runningText.setText("Running");
 	      }
+	  	running = !running;
 	}
 	
 	//public void sendDataPress(View view)
@@ -186,86 +197,137 @@ public class MainActivity extends Activity{
 }
 
 
-class myLocationListener implements LocationListener{
-	private Time now;
-	private double latitude;
-	private double longitude;
-	private LocationManager lm;
-	private String provider;
-	private Location loc;
-	private Context mainContext;
-	private ArrayList<imu> rowArray = new ArrayList<imu>(); //trimToSize() at the end
-	private ListView dataText;
-	private ArrayList<String> displayData = new ArrayList<String>(1);
-	public myLocationListener (Context mainCntxt, ListView dataTxt)
-	{
-		mainContext = mainCntxt;
-		dataText = dataTxt;
-		lm = (LocationManager)mainContext.getSystemService(Context.LOCATION_SERVICE);
-		provider = lm.getBestProvider(new Criteria(), false);
-		loc = lm.getLastKnownLocation(provider);
-		if (loc != null)
-		{
-			System.out.println("GPS connected\n");
-		}
+class myListener implements LocationListener, SensorEventListener{
+	 private SensorManager mSensorManager;
+     private Sensor sAccelerometer;
+     private Sensor sGyro;
+     private Sensor sGravity;
+     private Sensor sLinAccel;
+     private boolean hasGyro = false;
+     private Sensor sRotateVec;
+     
+     private boolean gotSensor = true;
+	 private boolean takeReadings = false;
+     private boolean enableSensors = true;
+     private boolean hasSpeed = false;
+     private boolean hasBearing = false;
+     
+     private imu currentData;
+     
+     private LocationManager lm;
+     private boolean gotGPS = false;
+     private String provider;
+     private Location loc;
+     private Context mainContext;
+     private ArrayList<imu> rowArray = new ArrayList<imu>(); //trimToSize() at the end
+     private ListView dataText;
+     private ArrayList<String> displayData = new ArrayList<String>(1);
+     @SuppressLint("InlinedApi")
+	public myListener (Context mainCntxt, ListView dataTxt, SensorManager sensorM)
+     {
+    	 currentData = new imu();
+    	 mainContext = mainCntxt;
+    	 dataText = dataTxt;
+    	 lm = (LocationManager)mainContext.getSystemService(Context.LOCATION_SERVICE);
+    	 provider = LocationManager.GPS_PROVIDER;
+    	 loc = lm.getLastKnownLocation(provider);
+    	 if (loc != null)
+    	 {
+    		 System.out.println("GPS connected\n");
+		}	
 		else
-			System.out.println("null\n");
-	}
-	
-	@SuppressLint("NewApi")
-	public void getCoordinates()
-	{
-	  	lm.requestSingleUpdate(provider, this, null);
-	}
-	
-	
-	@Override
-	  public void onLocationChanged(Location loc) {
-		imu currentData = new imu();
-		currentData.latitude = loc.getLatitude();
-		currentData.longitude = loc.getLongitude();
-		currentData.speed = loc.getSpeed();
-		currentData.bearing = loc.getBearing();
-		currentData.time = loc.getTime();
-		rowArray.add(currentData);
-		displayData.add(Double.toString(currentData.latitude) + ", " + Double.toString(currentData.longitude)
-				+ ";  " + Double.toString(currentData.speed) + ", " + Double.toString(currentData.bearing));
-    dataText.setAdapter(new ArrayAdapter<String>(mainContext, android.R.layout.simple_list_item_1, displayData));	
-
-	  }
-	
-	
-	public void sendData(String ip, String port)
-	{
-		//disable buttons while sending data
-		int rowSize = rowArray.size();
-		if (rowSize != 0)
-		{
-			new talkToServer().execute(ip, port);
-		}
-		else
-			Toast.makeText(mainContext, "No data to send.", Toast.LENGTH_SHORT).show();
+		System.out.println("null\n");
 		
-	}
+		if (enableSensors)
+		{	
+			mSensorManager = sensorM;
+			sAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+			sGyro = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+			sRotateVec = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+			sLinAccel = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+			sGravity = mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
 	
-	 private class talkToServer extends AsyncTask<String,Void,Void>
-		{
-			@Override
-			protected Void doInBackground(String... ip_addr) {
-				Socket connection = null;
-				try {
-					System.out.println("opening socket...");
-					
-					connection = new Socket(ip_addr[0], Integer.valueOf(ip_addr[1]));
-					
-					ObjectOutputStream output = new ObjectOutputStream(connection.getOutputStream());
-					output.writeObject(rowArray);
-					output.flush();
-					
-					output.close();
-					
-					connection.close();
-					
+			if (mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)==null)
+			{
+		    	 Toast.makeText(mainContext, "Gyroscope unvailable.", Toast.LENGTH_LONG).show();	
+		    	 hasGyro = false;
+			}
+			else
+				hasGyro = true;
+		}
+     }
+     
+     @SuppressLint("NewApi")
+     public void getCoordinates(boolean takeReadings)
+     {
+    	 this.takeReadings = takeReadings;
+    	 lm.requestSingleUpdate(provider, this, null);
+    	 
+    	 if (enableSensors & takeReadings)
+    	 {
+	    	 mSensorManager.registerListener(this, sAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+	    	 //mSensorManager.registerListener(this, sRotateVec, SensorManager.SENSOR_DELAY_NORMAL);
+	    	 //if (hasGyro)
+	    	//	 mSensorManager.registerListener(this, sGyro, SensorManager.SENSOR_DELAY_NORMAL);
+	    	 //mSensorManager.registerListener(this, sLinAccel, SensorManager.SENSOR_DELAY_NORMAL);
+	    	 //mSensorManager.registerListener(this, sGravity, SensorManager.SENSOR_DELAY_NORMAL);
+    	 }
+    	 System.out.println("GPS requested.");
+     }
+	
+	
+     @Override
+     public void onLocationChanged(Location loc) {
+    	 if (takeReadings)
+    	 {
+	    	 System.out.println("Gps acquired.");
+	    	 gotGPS = true;
+	    	 currentData.latitude = loc.getLatitude();
+	    	 currentData.longitude = loc.getLongitude();
+	    	 currentData.speed = loc.getSpeed();
+	    	 System.out.println(loc.getSpeed() + ", " + loc.getBearing());
+	    	 hasSpeed = loc.hasSpeed();
+	    	 hasBearing = loc.hasBearing();
+	    	 System.out.println(hasSpeed + ", " + hasBearing);
+	    	 currentData.bearing = loc.getBearing();
+	    	 currentData.time = loc.getTime();
+	    	 
+	    	 if (gotSensor | !enableSensors)
+	    		 makeEntry();
+    	 }
+     }
+	
+	
+     public void sendData(String ip, String port)
+     {
+    	 //disable buttons while sending data
+    	 int rowSize = rowArray.size();
+    	 if (rowSize != 0)
+    	 {
+    		 new talkToServer().execute(ip, port);
+    	 }	
+    	 else
+    		 Toast.makeText(mainContext, "No data to send.", Toast.LENGTH_SHORT).show();	
+     }
+     
+     private class talkToServer extends AsyncTask<String,Void,Void>
+     {
+    	 @Override
+    	 protected Void doInBackground(String... ip_addr) {
+    		 Socket connection = null;
+    		 try {
+    			 System.out.println("opening socket...");
+    			 
+    			 connection = new Socket(ip_addr[0], Integer.valueOf(ip_addr[1]));
+    			 
+    			 ObjectOutputStream output = new ObjectOutputStream(connection.getOutputStream());
+    			 output.writeObject(rowArray);
+    			 output.flush();
+    			 
+    			 output.close();
+    			 
+    			 connection.close();
+    			 
 				} catch (UnknownHostException e ) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -280,21 +342,95 @@ class myLocationListener implements LocationListener{
 		}
 		
 
-	  @Override
-	  public void onStatusChanged(String provider, int status, Bundle extras) {
-	    // TODO Auto-generated method stub
+     @Override
+     public void onStatusChanged(String provider, int status, Bundle extras) {
+    	 // TODO Auto-generated method stub
+    	 
+	  }	
+
+     @Override
+     public void onProviderEnabled(String provider) {
 
 	  }
 
-	  @Override
-	  public void onProviderEnabled(String provider) {
+     @Override
+     public void onProviderDisabled(String provider) {
 
-	  }
+     }   
 
-	  @Override
-	  public void onProviderDisabled(String provider) {
+     public void onAccuracyChanged(Sensor sensor, int accuracy) {
+     }
 
-	  }
+     public void onSensorChanged(SensorEvent event) {
+    	 Sensor thisSensor = event.sensor;
+    	 System.out.println("Sensor event.");
+
+    	 float [] values = event.values;
+    	 switch(thisSensor.getType())
+    	 {
+    	 	case Sensor.TYPE_ACCELEROMETER:
+    	 		System.out.println("Accelerometer data acquired.");
+    	 		currentData.accelX = values[0];
+    	 		currentData.accelY = values[1];
+    	 		currentData.accelZ = values[2];
+    	 		mSensorManager.unregisterListener(this);
+    	 		if (hasGyro)
+    	 			mSensorManager.registerListener(this, sGyro, SensorManager.SENSOR_DELAY_NORMAL);
+    	 		else
+    	 			mSensorManager.registerListener(this, sRotateVec, SensorManager.SENSOR_DELAY_NORMAL);
+    	 		break;
+    	 	case Sensor.TYPE_GYROSCOPE:
+    	 		System.out.println("Gyroscope data acquired.");
+    	 		currentData.gyroX = values[0];
+    	 		currentData.gyroY = values[1];
+    	 		currentData.gyroZ = values[2];
+    	 		mSensorManager.unregisterListener(this);
+    	 		mSensorManager.registerListener(this, sRotateVec, SensorManager.SENSOR_DELAY_NORMAL);
+    	 		break;
+    	 	case Sensor.TYPE_ROTATION_VECTOR:
+    	 		System.out.println("Rotation vector data acquired.");
+    	 		currentData.rotVecX = values[0];
+    	 		currentData.rotVecY = values[1];
+    	 		currentData.rotVecZ = values[2];
+    	 		//float orientValues[] = new float[3];
+    	 		//SensorManager.getOrientation(values, orientValues);
+    	 		currentData.orientationA = 0.0f;//orientValues[0];
+    	 		currentData.orientationB = 0.0f;//orientValues[1];
+    	 		currentData.orientationC = 0.0f;//orientValues[2];	
+    	 		mSensorManager.unregisterListener(this);
+    	 		mSensorManager.registerListener(this, sLinAccel, SensorManager.SENSOR_DELAY_NORMAL);
+    	 		break;
+    	 	case Sensor.TYPE_LINEAR_ACCELERATION:
+    	 		System.out.println("Linear acceleration data acquired.");
+    	 		currentData.linAccX = values[0];
+    	 		currentData.linAccY = values[1];
+    	 		currentData.linAccZ = values[2];
+    	 		mSensorManager.unregisterListener(this);
+    	 		mSensorManager.registerListener(this, sGravity, SensorManager.SENSOR_DELAY_NORMAL);
+    	 		break;
+    	 	case Sensor.TYPE_GRAVITY:
+    	 		System.out.println("Gravity data acquired.");
+    	 		currentData.gravityX = values[0];
+    	 		currentData.gravityY = values[1];
+    	 		currentData.gravityZ = values[2];
+    	 		 System.out.println("Got all sensors");
+        		 gotSensor = true;
+        		 mSensorManager.unregisterListener(this);
+        		 if (gotGPS)    			 
+        			 makeEntry();
+    	 		break;
+    	 		
+    	 }
+     }
+     
+     private void makeEntry(){
+    	 gotGPS = false;
+    	 rowArray.add(currentData);
+    	 displayData.add(Double.toString(currentData.latitude) + ", " + Double.toString(currentData.longitude)
+				+ ";  " + Double.toString(currentData.speed) + ", " + Double.toString(currentData.bearing) + ", " + Boolean.toString(hasSpeed) + ", " + Boolean.toString(hasBearing));
+    	 dataText.setAdapter(new ArrayAdapter<String>(mainContext, android.R.layout.simple_list_item_1, displayData));	
+    	 currentData = new imu();
+     }
 
 }
 
