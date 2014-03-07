2ee841feacce96e3b24d9426c95b198c9fc38b05
Jeff Terrell <jdterrell@neo.tamu.edu>
2014-03-07 00:11:56 -0600

    It works!

--- Project2/TeamRamRod/algorithm_test/src/com/jdterrell/test/Algorithm.java
+++ Project2/TeamRamRod/algorithm_test/src/com/jdterrell/test/Algorithm.java
@@ -2,6 +2,24 @@ package com.jdterrell.test;
 
 import java.lang.Math;
 import java.sql.*;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.ByteArrayContent;
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpContent;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.fusiontables.Fusiontables;
+import com.google.api.services.fusiontables.FusiontablesScopes;
 
 //import com.jdterrell.p2.AndroidPacket2;
 
@@ -15,7 +33,34 @@ import java.sql.*;
 
 public class Algorithm {
 	
-	static double t_sample = 100;	//10 seconds between every measurement point
+	private static final String APPLICATION_NAME = "ECEN 489 Project Two";
+    private static final java.io.File DATA_STORE_DIR =
+            new java.io.File(System.getProperty("user.home"), ".store/fusiontables_sample");
+    private static FileDataStoreFactory dataStoreFactory;
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static HttpTransport httpTransport;
+	
+    @SuppressWarnings("unused")
+    private static Fusiontables client;
+    
+    private static Credential authorize() throws Exception {
+
+        Set<String> scopes = new HashSet<String>();
+        scopes.add(FusiontablesScopes.FUSIONTABLES);
+        scopes.add(FusiontablesScopes.FUSIONTABLES_READONLY);
+
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                httpTransport, 
+                JSON_FACTORY, 
+                "391485295951.apps.googleusercontent.com",
+                "478DjK9lvUho00AcpJrImzgq", 
+                scopes).setDataStoreFactory(
+                dataStoreFactory).build();
+        return new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
+    }
+    
+    
+	//static double t_sample = 10;	//10 seconds between every measurement point
 	static double R = 6731000; //mean radius of Earth in meters
 	static long time1;
 	static long time2;
@@ -29,11 +74,21 @@ public class Algorithm {
 	static double distance;
 	static double delta_lat;
 	static double delta_lon;
+	static String sql3;
+	static String sql4;
 	
 	public static void main(String args[])	{
 		Connection c = null;
 		Statement stmt = null;
 		try {
+
+			//Fusion Table setup code
+			httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+            dataStoreFactory = new FileDataStoreFactory(DATA_STORE_DIR);
+            Credential credential = authorize();
+            client = new Fusiontables.Builder(httpTransport, JSON_FACTORY, credential)
+                    .setApplicationName(APPLICATION_NAME).build();
+            
 			Class.forName("org.sqlite.JDBC");
 			c = DriverManager.getConnection("jdbc:sqlite:C:/projtwo.db");
 		      c.setAutoCommit(false);
@@ -42,13 +97,12 @@ public class Algorithm {
 		      stmt = c.createStatement();
 		      
 		      ResultSet rs = stmt.executeQuery( "SELECT * FROM ecen489_project2_data;" );
-//		      ResultSet.rs1 = stmt.executeQuery();
 		      
 		      while (rs.next())	{
 		         
 		    	 int id = rs.getInt(1);
 		    	 
-		         if (id > 9) {
+		         if (id > 4) {
 		        	 break;
 		         }
 		         
@@ -56,7 +110,7 @@ public class Algorithm {
 //		         for(int i = 1; i<=10; i++) {
 		        	 
 		        	 //get the next 9 points
-			         for(int j = 2; j<=8; j++) {
+			         for(int j = 0; j<=4; j++) {
 			         
 				         if (id == j) {
 					         System.out.println();
@@ -66,7 +120,7 @@ public class Algorithm {
 					         longitude1 = deg2rad(rs.getDouble(4));		        	 
 					         System.out.println( "Time = " + time1 );
 					         System.out.println( "Latitude = " + rad2deg(latitude1) );
-					         System.out.println( "Longitude = " + rad2deg(longitude1) );		        	 
+					         System.out.println( "Longitude = " + rad2deg(longitude1) );
 				         }
 				         
 				         else if (id == (j+1)) {
@@ -98,6 +152,48 @@ public class Algorithm {
 					         System.out.println("Delta_Latitude = " + delta_lat);
 					         System.out.println("Delta_Longitude = " + delta_lon);
 					         System.out.println();
+					        
+					         //push calculation back to database
+					         String sql1 = "UPDATE ecen489_project2_data set latitude = " + latitude2 + " where _id=" + (j+1) + ";'";
+					         stmt.execute(sql1);
+					         String sql2 = "UPDATE ecen489_project2_data set longitude = " + longitude2 + " where _id=" + (j+1) + ";'";
+					         stmt.execute(sql2);
+					         System.out.println("Row " + id + "updated!");					   
+					         c.commit();
+					         
+					         sql3 = "INSERT INTO 1kMI1PaLFXtXuP06qk9SX7nARPeLehs1Jw5agd1M"+ "("
+					            		+ "time, "
+					            		+ "latitude,"
+					            		+ "longitude,"
+					            		+ "bearing,"
+					            		+ "speed" + 
+					            		") VALUES ("
+					            		+ time1 + ", "
+					            		+ latitude1 + ", "
+					            		+ longitude1 + ", "
+					            		+ bearing + ", "
+					            		+ speed + ");";
+					            
+					         sql4 = "INSERT INTO 1kMI1PaLFXtXuP06qk9SX7nARPeLehs1Jw5agd1M"+ " ("
+					            		+ "time, "
+					            		+ "latitude,"
+					            		+ "longitude,"
+					            		+ "bearing,"
+					            		+ "speed" + 
+					            		") VALUES ("
+					            		+ time2 + ", "
+					            		+ latitude2 + ", "
+					            		+ longitude2 + ", "
+					            		+ bearing + ", "
+					            		+ speed + ");";
+					         
+					            HttpContent content1 = ByteArrayContent.fromString(null, "sql=" + sql3);
+					            HttpRequest httpRequest1 = client.getRequestFactory().buildPostRequest(new GenericUrl("https://www.googleapis.com/fusiontables/v1/query"), content1);
+					            httpRequest1.execute();
+					            
+					            HttpContent content2 = ByteArrayContent.fromString(null, "sql=" + sql4);
+					            HttpRequest httpRequest2 = client.getRequestFactory().buildPostRequest(new GenericUrl("https://www.googleapis.com/fusiontables/v1/query"), content2);
+					            httpRequest2.execute();
 				         }
 			         }
 //		         }
