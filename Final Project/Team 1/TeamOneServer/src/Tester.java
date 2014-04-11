c63c121d66d7afcc3014a362dd211f157f9bd2c6
Zachary Partal <zpartal@gmail.com>
2014-04-11 16:45:03 -0500

    more work on angle calc

--- Final Project/Team 1/TeamOneServer/src/Tester.java
+++ Final Project/Team 1/TeamOneServer/src/Tester.java
@@ -46,14 +46,14 @@ public class Tester {
         new Thread(new FusionTableHandler(dataset)).start();*/
 
 //        new Thread(new ServoDriver()).start();
-        double base_lat = 30.623399;
-        double base_lon = -96.335571;
-        double cal_lat = 30.623698;
-        double cal_lon = -96.334913;
-        double trc_lat = 30.623566;
-        double trc_lon = -96.334748;
+        double base_lat = 30.7;
+        double base_lon = -96.0;
+        double cal_lat = 30.5;
+        double cal_lon = -96.5;
+        double trc_lat = 30.4;
+        double trc_lon = -96.8;
 
-        int bearing = (int) getAngle(base_lat, base_lon, cal_lat, cal_lon, trc_lat, trc_lon);
+        int bearing = (int) getMotorAngle(base_lat, base_lon, cal_lat, cal_lon, trc_lat, trc_lon);
         System.out.println(bearing);
 
 //        SerialHandler sh = new SerialHandler();
@@ -71,9 +71,64 @@ public class Tester {
 
     }
 
+    public static double getMotorAngle(double base_lat, double base_lon, double cal_lat, double cal_lon, double trc_lat, double trc_lon) {
+        int calquad = getQuadrant(cal_lat,cal_lon,base_lat,base_lon);
+        int trcquad = getQuadrant(cal_lat,cal_lon,trc_lat,trc_lon);
+
+
+
+        double alpha = getAlphaBeta(base_lat,base_lon,cal_lat,cal_lon);
+        double beta = getAlphaBeta(trc_lat,trc_lon,cal_lat,cal_lon);
+
+        if(calquad == trcquad){
+            return getAngle(base_lat,base_lon,cal_lat,cal_lon,trc_lat,trc_lon);
+        }else if((int)Math.abs((double)(calquad-trcquad))%4 == 2){
+            return (180.0 + getAngle(base_lat,base_lon,cal_lat,cal_lon,trc_lat,trc_lon));
+        }else if(((trcquad == 1 && calquad == 4)) || ((trcquad > calquad) && (trcquad != 4))){
+            if(beta > alpha){
+                return (180.0 + getAngle(base_lat,base_lon,cal_lat,cal_lon,trc_lat,trc_lon));
+            }else{
+                return getAngle(base_lat,base_lon,cal_lat,cal_lon,trc_lat,trc_lon);
+            }
+        }else{
+            if(beta > alpha){
+                return  -1.0*getAngle(base_lat,base_lon,cal_lat,cal_lon,trc_lat,trc_lon);
+            }else{
+                return (180.0 - getAngle(base_lat,base_lon,cal_lat,cal_lon,trc_lat,trc_lon));
+            }
+        }
+    }
+
+    public static double getAlphaBeta(double base_lat, double base_lon, double cal_lat, double cal_lon) {
+        int calquad = getQuadrant(cal_lat,cal_lon,base_lat,base_lon);
+
+        double alpha = 0.0;
+
+        switch(calquad){
+            case 1:
+                alpha = getAngle(base_lat,base_lon, cal_lat,cal_lon,cal_lat,base_lon);
+                break;
+            case 2:
+                alpha = getAngle(base_lat,base_lon, cal_lat,cal_lon,base_lat,cal_lon);
+                break;
+            case 3:
+                alpha = getAngle(base_lat,base_lon, cal_lat,cal_lon,cal_lat,base_lon);
+                break;
+            case 4:
+                alpha = getAngle(base_lat,base_lon, cal_lat,cal_lon,base_lat,cal_lon);
+                break;
+            default:
+                alpha = 0.0;
+        }
+        return alpha;
+    }
+
     public static double getAngle(double base_lat, double base_lon, double cal_lat, double cal_lon, double trc_lat, double trc_lon) {
         double cal_base_lat = Math.toRadians(base_lat) - Math.toRadians(cal_lat);
         double cal_base_lon = Math.toRadians(base_lon) - Math.toRadians(cal_lon);
+
+
+
         double cal_base_mag = Math.sqrt(cal_base_lat*cal_base_lat+cal_base_lon*cal_base_lon);
 
         double cal_trc_lat = Math.toRadians(trc_lat) - Math.toRadians(cal_lat);
@@ -81,7 +136,25 @@ public class Tester {
         double cal_trc_mag = Math.sqrt(cal_trc_lat*cal_trc_lat+cal_trc_lon*cal_trc_lon);
 
         double return_ang = Math.asin((cal_base_lat*cal_trc_lon - cal_base_lon*cal_trc_lat)/(cal_base_mag*cal_trc_mag));
+       // System.out.println("angle "+return_ang);
         return Math.toDegrees(return_ang);
+    }
+
+    //quadrant of any vector w/ respect to the cantenna
+    public static int getQuadrant(double cal_lat, double cal_lon, double trc_lat, double trc_lon){
+        double cal_base_lat = Math.toRadians(trc_lat) - Math.toRadians(cal_lat);
+        double cal_base_lon = Math.toRadians(trc_lon) - Math.toRadians(cal_lon);
+
+//       System.out.println("lat "+cal_base_lat+" lon "+cal_base_lon);
+        if((cal_base_lat >= 0) && (cal_base_lon >= 0)){
+            return 1;
+        }else if((cal_base_lat >= 0) && (cal_base_lon < 0)){
+            return 2;
+        }else if((cal_base_lat < 0) && (cal_base_lon < 0)){
+            return 3;
+        }else{
+            return 4;
+        }
     }
 
     public static double calculateBearing(double lat1, double lat2, double long1, double long2) {
