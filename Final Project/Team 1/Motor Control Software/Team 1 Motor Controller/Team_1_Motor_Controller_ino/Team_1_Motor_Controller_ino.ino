de7cdcb3ba1f7f5120e793fcc8b51578c23795fa
Jeff Terrell <jdterrell@neo.tamu.edu>
2014-04-15 15:09:26 -0500

    Commented Motor Control Code

--- Final Project/Team 1/Motor Control Software/Team 1 Motor Controller/Team_1_Motor_Controller_ino/Team_1_Motor_Controller_ino.ino
+++ Final Project/Team 1/Motor Control Software/Team 1 Motor Controller/Team_1_Motor_Controller_ino/Team_1_Motor_Controller_ino.ino
@@ -19,20 +19,23 @@ int start_flag;
 
 void setup()
 {
-
+	//digital output pins
     pinMode(DIR, OUTPUT);
     pinMode(EN, OUTPUT);
     
-    Serial.begin(9600);
+    //configure serial
+	//Serial -> internal communication handled here (want to keep data we are passing around clear)
+	//Serial1 -> reserved for debugging and/or verification output
+	Serial.begin(9600);
     Serial1.begin(9600);
-    Serial1.print("Setup complete...");
-    
 
+	//initialize digital pins
     digitalWrite(DIR, LOW);
     digitalWrite(EN, LOW);
     
-    analogWriteFrequency(PULSE, 400);
-    analogWrite(PULSE, 255);   
+	//configure PULSE pin for PWM control
+    analogWriteFrequency(PULSE, 400);	//pulse frequency
+    analogWrite(PULSE, 255);   		//duty cycle represented from 0-255
     
     pos = 0;
     start_flag = 0;
@@ -40,14 +43,18 @@ void setup()
 
 void loop()
 { 
-    angle = 0;
+    //initialize
+	angle = 0;
     number = 0;
     
+	//build command string from user input
     while (Serial.available()) {
       char c = Serial.read();
       command += c;
       delay(2);
     }
+	
+	//convert serial string to int
     if(command.length() > 0) {
       Serial1.println(command);
       angle = command.toInt();
@@ -56,52 +63,67 @@ void loop()
       Serial.flush();
     }    
 
+	//start checking user input to determine course of action	
     if(start_flag){
+	  
+	  // range of motion is between -180 and 180 degrees
+	  // server will only send values within this range
       if(abs(angle) > 180) {    
-        Serial1.println("Out of Range!");
+        
+		Serial1.println("Out of Range!");	//just in case...
       }
        
       else {
         
+		//new angle calculation using memory of last (current) position
         angle = angle - pos;
-        Serial.print("Angle: ");
-        Serial.println(angle);
-        
+		       
+		//calculate number of steps (pulses)
         number = angle/0.09;
-        Serial.print("Number: ");
-        Serial.println(number);        
-        rotate(-number);
-        
-        Serial.print("Old Position: ");
-        Serial.println(pos);        
+		
+		//rotate by number of steps
+        rotateStepper(-number);
+                
+		//calculate new position
         pos = pos + number * 0.09;
-        Serial.print("Position: ");
-        Serial.println(pos);
-        Serial.println("-----------");        
+
+//debugging outputs, uncomment and monitor as needed		
+//        Serial.print("Angle: ");
+//        Serial.println(angle);		
+//        Serial.print("Number: ");
+//        Serial.println(number);        
+//        Serial.print("Position: ");
+//        Serial.println(pos);
+//        Serial.println("-----------");		
+       
       }
 ;
         
-      Serial1.println("");
-      Serial1.print("Antenna position: ");
-      Serial1.print(pos);
-      Serial1.print(" degrees");
-      Serial.flush();
+//      Serial1.println("");
+//      Serial1.print("Antenna position: ");
+//      Serial1.print(pos);
+//      Serial1.print(" degrees");
+
+      Serial.flush();	//flush to prepare for new data
       
-      start_flag = 0;
+      start_flag = 0;	//finished with process, reset start flag
     }
 }
 
-// rotate by step numbers
-void rotate(int number)
+//function to determine which direction in which to rotate 
+//and rotate by number of steps calculated in main
+void rotateStepper(int number)
 {
-    if(number >= 0)
-      digitalWrite(DIR, LOW);
-    else
-    {
-      digitalWrite(DIR, HIGH);
-      number = -number;
+    if(number >= 0)	{
+		digitalWrite(DIR, LOW);	//set direction to CW
+    } 
+	else {
+		digitalWrite(DIR, HIGH); //set direction to CCW
+		number = -number;		//might be redundant, but since direction 
+								//is controlled by state of DIR pin, need a positive step number
     }
-    
+
+	//small delay before 
     delay(1);
     
     for(int i=0; i < number; i++)
@@ -111,16 +133,18 @@ void rotate(int number)
       //set PWM Duty Cycle to 1
       analogWrite(PULSE, 255);
       
-      // 1 millisecond pulse
+      // 1 millisecond pulse length
       delay(1);
       
-      //disable motor
+      //disable motor -- necessary to avoid erratic behavior
       digitalWrite(EN, LOW);
 
       
     }
 }
 
+//might not need this function...never called
+/*
 int cal_steps(double angle, double pos)
 {
     int number = 0;
@@ -129,3 +153,4 @@ int cal_steps(double angle, double pos)
     else
       return 0;
 }
+*/
